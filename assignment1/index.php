403f85f36fcc1056c43dcd067722693e680b0ca1
Mark Biesheuvel <mail@markbiesheuvel.nl>
2016-06-29 22:07:06 +0200

    Add some Bootstrap styling

--- assignment1/index.php
+++ assignment1/index.php
@@ -8,6 +8,12 @@ require_once(__DIR__ . '/include.php');
 $period = intval(post('period', 12)); // Life-Time of 12 months
 $commission = floatval(post('commission', 10)); // 10% commission
 
+// In range
+$period = max(0, $period);
+
+$commission = max(0, $commission);
+$commission = min(100, $commission);
+
 $periodOptions = [3, 6, 12, 18, 24];
 
 // Prepare query
@@ -49,78 +55,67 @@ $result = $db
 	<head>
 		<title>Assignment 1: Create a Report (SQL)</title>
 		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
-		<style type="text/css">
-			.report-table
-			{
-				width: 100%;
-				border: 1px solid #000000;
-			}
-			.report-table td,
-			.report-table th
-			{
-				text-align: left;
-				border: 1px solid #000000;
-				padding: 5px;
-			}
-			.report-table .right
-			{
-				text-align: right;
-			}
-		</style>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 	</head>
 	<body>
-		<h1>Report:</h1>
+		<div class="container">
 
-		<form action="?" method="post">
-			<dl>
-				<dt>Period</dt>
-				<dd>
-					<select name="period">
-						<?php foreach($periodOptions as $option): ?>
-							<option value="<?= $option?>" <?= ($period === $option)? 'selected' : '' ?>>
-								<?= $option ?> months
-							</option>
-						<?php endforeach; ?>
-					</select>
-				</dd>
-				<dt>Commision</dt>
-				<dd>
-					<input type="number" name="commission" value="<?= $commission ?>"> %
-				</dd>
-				<dt>&nbsp;</dt>
-				<dd><input type="submit"></dd>
-			</dl>
-		</form>
+			<h1>Report</h1>
 
-		<hr>
+			<form action="?" method="post">
+				<div class="row">
+					<div class="col-md-4">
+						<div class="input-group">
+							<span class="input-group-addon">Period</span>
+							<select name="period" class="form-control">
+								<?php foreach($periodOptions as $option): ?>
+									<option value="<?= $option?>" <?= ($period === $option)? 'selected' : '' ?>>
+										<?= $option ?> months
+									</option>
+								<?php endforeach; ?>
+							</select>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<div class="input-group">
+							<span class="input-group-addon">Commision</span>
+							<input type="number" class="form-control" name="commission" value="<?= $commission ?>" min="0" max="100" step="any">
+							<span class="input-group-addon">%</span>
+						</div>
+					</div>
+					<div class="col-md-4">
+						<input type="submit" class="btn btn-success">
+					</div>
+			</form>
 
-		<table class="report-table">
-			<thead>
-				<tr>
-					<th>Start</th>
-					<th class="right">Bookers</th>
-					<th class="right"># of bookings (avg)</th>
-					<th class="right">Turnover (avg)</th>
-					<th class="right">LTV</th>
-				</tr>
-			</thead>
-			<tbody>
-				<?php foreach ($result as $index => $row): ?>
+			<table class="table">
+				<thead>
 					<tr>
-						<td><?= $row->first_month ?></td>
-						<td class="right"><?= $row->total_bookers ?></td>
-						<td class="right"><?= number_format($row->total_number_of_bookings / $row->total_bookers, 1) ?></td>
-						<td class="right"><?= currency($row->total_revenue / $row->total_bookers) ?></td>
-						<td class="right"><?= currency($row->total_revenue / $row->total_bookers * $commission / 100) ?></td>
+						<th>Start</th>
+						<th class="text-right">Bookers</th>
+						<th class="text-right"># of bookings (avg)</th>
+						<th class="text-right">Turnover (avg)</th>
+						<th class="text-right">LTV</th>
 					</tr>
-				<?php endforeach; ?>
-			</tbody>
-			<tfoot>
-				<tr>
-					<td colspan="4" class="right"><strong>Total rows:</strong></td>
-					<td><?= $index + 1 ?></td>
-				</tr>
-			</tfoot>
-		</table>
+				</thead>
+				<tbody>
+					<?php foreach ($result as $index => $row): ?>
+						<tr>
+							<td><?= $row->first_month ?></td>
+							<td class="text-right"><?= $row->total_bookers ?></td>
+							<td class="text-right"><?= number_format($row->total_number_of_bookings / $row->total_bookers, 1) ?></td>
+							<td class="text-right"><?= currency($row->total_revenue / $row->total_bookers) ?></td>
+							<td class="text-right"><?= currency($row->total_revenue / $row->total_bookers * $commission / 100) ?></td>
+						</tr>
+					<?php endforeach; ?>
+				</tbody>
+				<tfoot>
+					<tr>
+						<td colspan="4" class="text-right"><strong>Total rows:</strong></td>
+						<td><?= $index + 1 ?></td>
+					</tr>
+				</tfoot>
+			</table>
+		</div>
 	</body>
 </html>
